d8cb60ed9a7bac415fca52d08cc6a3a7798f5a2c
Dimitri Dokuzovski <dimitri.dokuzovski@gmail.com>
2018-12-13 17:01:28 +0100

    da se dopraet narackite

--- app/pages/orders/directives/orders-directive.js
+++ app/pages/orders/directives/orders-directive.js
@@ -1,7 +1,8 @@
 angular.module("app.pages.orders.directives.orders-directive", [])
     .directive("ordersDirective", ordersDirective)
 function ordersDirective() {
-
+    
+  
     return {
         restrict: "EA",
         // scope: {},
--- app/pages/orders/orders.js
+++ app/pages/orders/orders.js
@@ -1,21 +1,22 @@
 angular.module("app.pages.orders", [
     "app.pages.orders.directives.orders-directive"
 ])
-.config(config)
-.controller("ordersController", ordersController)
+    .config(config)
+    .controller("ordersController", ordersController)
 
 
 
 
-function ordersController($scope,$timeout) {
+function ordersController($scope, $timeout) {
     let orders = this;
-    
-   
-    $scope.hoverIn = function(artikl){
-            artikl.status = true;
+    orders.naracaniArtikli = []
+
+
+    orders.hoverIn = function (artikl) {
+        artikl.status = true;
     };
-    
-    $scope.hoverOut = function(artikl){
+
+    orders.hoverOut = function (artikl) {
         artikl.status = false;
 
     };
@@ -31,9 +32,60 @@ function ordersController($scope,$timeout) {
         { ime: "Еспресо", id: 7, kolicina: 1, opis: "0,15л" },
         { ime: "Швепс", id: 8, kolicina: 1, opis: "0.33л" }
     ]
- 
 
-    
+
+    orders.incrementArtikl = function (artikl) {
+        for (let artikl of orders.artikli) {
+            artikl.kolicina++;
+        }
+    };
+
+    orders.decrementArtikl = function (artikl, artiklId) {
+        for (let artikl of orders.artikli) {
+            if (artikl.kolicina > 1) {
+                artikl.kolicina--;
+            }
+        }
+    };
+
+    orders.checkIfArtiklExists = function (id) {
+        for (let artikl of orders.artikli) {
+            if (artikl.id === id) {
+                return 1
+            }
+        }
+        return 0;
+    }
+
+    orders.addNaracki = function (artiklId) {
+        if (orders.checkIfArtiklExists(artiklId)) {
+            for (let artikl of orders.artikli) {
+                if (artikl.id === artiklId) {
+                    artikl.kolicina++;
+                }
+            }
+        }
+        else {
+            orders.naracaniArtikli.push(orders.artikli);
+
+        }
+    }
+
+    orders.deleteArtikl = function (index) {
+        orders.naracaniArtikli.splice(index, 1);
+    }
+
+
+
+
+    // orders.deleteArtikl = function () {
+    //     let artiklToDelete = orders.artikl
+    //     for (let i = 0; i < orders.artikli.length; i++) {
+    //         if (artiklToDelete === orders.artikli[i]) {
+    //             orders.artikli.splice(i, 1);
+    //         }
+    //     }
+    // }
 }
 
 function config($stateProvider) {
